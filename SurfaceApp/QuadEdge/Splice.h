69036ff3a2ff543437d5945eac4962a06ed8ee45
Anton Terekhov <aterekhov@aligntech.com>
2014-04-27 07:13:27 +0400

    Add even move splice versions

--- SurfaceApp/QuadEdge/Splice.h
+++ SurfaceApp/QuadEdge/Splice.h
@@ -11,7 +11,9 @@ namespace Quad_NS
   //  overrides b.o and b.l edge rings
   template <typename Core> void spliceAA( Leaf<Core>& a, Leaf<Core>& b )
   {
+    //  preserves a.l, overrides b.l
     b.oNext().rot().fuse( a.oNext().rot() );
+    //  preserves a.o, overrides b.o
     b.fuse( a );
   }
 
@@ -21,12 +23,38 @@ namespace Quad_NS
   //  overrides b.o and a.l edge rings
   template <typename Core> void spliceAB( Leaf<Core>& a, Leaf<Core>& b )
   {
+    //  preserves b.l, overrides a.l
     a.oNext().rot().fuse( b.oNext().rot() );
+    //  preserves a.o, overrides b.o
     b.fuse( a );
   }
 
 
-  /// default splice operation
+  //  topology operation
+  //  preserves b.o and a.l edge rings
+  //  overrides a.o and b.l edge rings
+  template <typename Core> void spliceBA( Leaf<Core>& a, Leaf<Core>& b )
+  {
+    //  preserves a.l, overrides b.l
+    b.oNext().rot().fuse( a.oNext().rot() );
+    //  preserves b.o, overrides a.o
+    a.fuse( b );
+  }
+
+
+  //  topology operation
+  //  preserves b.o and b.l edge rings
+  //  overrides a.o and a.l edge rings
+  template <typename Core> void spliceBB( Leaf<Core>& a, Leaf<Core>& b )
+  {
+    //  preserves b.l, overrides a.l
+    a.oNext().rot().fuse( b.oNext().rot() );
+    //  preserves b.o, overrides a.o
+    a.fuse( b );
+  }
+
+
+  // default version of splice operation
   template <typename Core> void splice( Leaf<Core>& a, Leaf<Core>& b ) { spliceAA( a, b ); }
 
 }
