bb36520480ac3432fb8fe4e2b1e6c6c343dbef48
Grif <_grif_@mail.ru>
2014-05-03 15:57:45 +0400

    Add Unit Test for Vector

--- SurfaceApp/UnitTests/Math.cpp
+++ SurfaceApp/UnitTests/Math.cpp
@@ -1,19 +1,54 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include "..\QuadEdge\Vector.h"
 #include "..\QuadEdge\Rational.h"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 
+using V = Math_NS::VectorI;
 using R = Math_NS::R;
 
 
-template <>
-std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString( const R& v ){ return std::to_wstring( v.n ) + std::wstring{ L'/' } +std::to_wstring( v.d ); }
+template <> std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString( const Math_NS::I& v )
+{
+  return std::to_wstring( v );
+}
+
+
+template <> std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString( const V& v )
+{
+  return L"( " + std::to_wstring( v.x ) + L", " + std::to_wstring( v.y ) + L", " + std::to_wstring( v.z ) + L" )";
+}
+
+template <> std::wstring Microsoft::VisualStudio::CppUnitTestFramework::ToString( const R& v )
+{
+  return std::to_wstring( v.n ) + L'/' +std::to_wstring( v.d );
+}
 
 
 namespace UnitTests
 {
+  TEST_CLASS( Vector )
+  {
+  public:
+
+    TEST_METHOD( Operatons )
+    {
+      Assert::AreEqual( V{ 1, 2, 3 }, V{ 1, 2, 3 }, L"Equality" );
+      Assert::AreEqual( V{ 7, 7, 7 }, V{ 1, 2, 3 } + V{ 6, 5, 4 }, L"Sum" );
+      Assert::AreEqual( V{ 5, 3, 1 }, V{ 6, 5, 4 } - V{ 1, 2, 3 }, L"Difference" );
+      Assert::AreEqual( 1LL, V{ 1, 0, 1 } * V{ 0, 1, 1 }, L"Scalar Product" );
+      Assert::AreEqual( 1LL * V{ 2, 4, 6 }, V{ 1, 2, 3 } *2LL, L"Multiplication" );
+      Assert::AreEqual( V{ 1, 2, 3 }, V{ 2, 4, 6 } / 2LL, L"Division" );
+      Assert::AreEqual( V{ 0, 0, 1 }, V{ 1, 0, 0 } ^ V{ 0, 1, 0 }, L"Vector Product" );
+      Assert::AreEqual( V{}, V{ 1, 2, 3 } + -V{ 1, 2, 3 }, L"Negation" );
+      Assert::AreEqual( 25LL, V{ 3, 0, 4 }.lengthSqr(), L"LengthSqr" );
+    }
+  };
+
+
   TEST_CLASS( Rational )
   {
   public:
